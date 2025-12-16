15d86081b578d077a4515aec3d83bf4c0188ec35
Yulian Tolkachov <56gymnasia@gmail.com>
2025-12-16 15:00:34 +0200

    feat:Basic enemy attack

--- scenes/main/enemy.gd
+++ scenes/main/enemy.gd
@@ -10,9 +10,20 @@ var player_chase: bool = false
 
 @onready var health: Health = $Health
 
+@export var DAMAGE := 20
+@export var ATTACK_COOLDOWN := 2.0
+@export var knockback_distance = 500
+var can_attack := true
+@onready var attack_area: Area2D = $AttackArea
+@onready var attack_cooldown: Timer = $AttackTimer
+
+
 func _ready():
 	health.died.connect(_on_health_died)
 	health.took_damage.connect(_on_health_took_damage)
+	
+	attack_area.body_entered.connect(_on_attack_area_body_entered)
+	attack_cooldown.timeout.connect(_on_attack_timer_timeout)
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -43,3 +54,21 @@ func _on_health_died() -> void:
 
 func _on_health_took_damage(amount: Variant) -> void:
 	pass # Replace with function body.
+
+
+func _on_attack_area_body_entered(body: Node2D) -> void:
+	if not can_attack: #attack cooldown is checked with a timer
+		return
+	
+	if body is Player:
+		body.health.take_damage(DAMAGE)
+		can_attack = false
+		attack_cooldown.start()
+		
+		body.velocity.x += sign(body.position.x - position.x) * knockback_distance #knockback
+
+func _on_attack_timer_timeout() -> void:
+	can_attack = true
+	for body in attack_area.get_overlapping_bodies():
+		if body is Player:
+			_on_attack_area_body_entered(body)
